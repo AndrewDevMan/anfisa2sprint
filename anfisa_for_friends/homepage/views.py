eb8153d1428b78142f9145bdcef46a4e1c730094
Андрей Тетнёв <andreytetnev@yandex.ru>
2023-06-09 15:18:55 +0300

    First commit

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    return render(
+        request=request,
+        template_name='homepage/index.html',
+        context={
+            'ice_cream_list': IceCream.objects.values(
+                'id', 'title', 'price', 'description',
+            ).filter(
+                is_on_main=True,
+                is_published=True,
+                category__is_published=True,
+            )
+        },
+    )
